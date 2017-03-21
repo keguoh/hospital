a61fb04178301d9ae414ebed56768baad2a7c872
keguoh <timmy0703@gmail.com>
2017-03-21 00:06:49 -0500

    normal

--- CallCenter/staffing/arrival_function.R
+++ CallCenter/staffing/arrival_function.R
@@ -1,6 +1,7 @@
+dat <- read.csv('C:/Users/Keguo/Dropbox/GitHub/queue-systems/CallCenter/staffing/Nov_Dec_data.csv', header = T)
+workdat <- dat[dat$arrival_day_of_week<5, ]
 #### Inital graphs ####
 attach(dat)
-
 # arrival time for one day / per hour
 d1 <- dat[arrival_day_of_week==1,]
 arrivalEpoch1 <- sort(d1$arrival_sec_of_day)
@@ -80,11 +81,9 @@ lines((28:95)/4+.125,as.vector(table(a5)), col=5, lwd=2)
 lines((28:95)/4+.125,as.vector(table(a6)), col=6, lwd=2)
 lines((28:95)/4+.125,as.vector(table(a0)), col=8, lwd=2)
 legend('topright', as.character(1:7), col=c(1:6,8), lwd=2, cex=.75)
-
-workdat <- dat[arrival_day_of_week<5, ]
 detach(dat)
-attach(workdat)
 
+attach(workdat)
 # three cases for workdays
 a = cut(workdat$arrival_sec_of_day, breaks = 3600*(28:96)/4)
 plot(0, ylab = "# of arrivals", xlab = "hours of day", type = 'n', 
@@ -105,7 +104,7 @@ plot(0, ylab = "# of arrivals", xlab = "hours of day", type = 'n',
 lines((7:23)+.5,as.vector(table(a)), col=1, lwd=2)
 
 
-
+attach(workdat)
 #### fit function ####
 X = aggregate(arrival_day_of_month, by=list(paste(month,arrival_day_of_month), arrival_day_of_week,arrival_hour_of_day), length)
 colnames(X) <- c('day', 'weekday', 'hour', 'arrivals')
@@ -115,14 +114,17 @@ X$hour3 <- (X$hour)^3
 X$hour4 <- (X$hour)^4
 X$hour5 <- (X$hour)^5
 X <- X[order(X$day), ]
+
+#### polynomials ####
 X1 <- X[X$weekday==1,]
 head(X1)
-plot(0, xlim = c(7,24), ylim = c(min(X1$arrivals), max(X1$arrivals)))
+plot(0, xlim = c(7,24), ylim = c(min(X1$arrivals), max(X1$arrivals)), 
+     main = 'polynomial for arrival rate function (MONDAYS)',
+     xlab = "Hours", ylab = '# of Arrivals')
 for(i in 1:(nrow(X1)/17)){
   lines(X1$hour[(i*17-16):(i*17)], X1$arrivals[(i*17-16):(i*17)])
 }
 
-## polynomials ##
 o1 <- lm(arrivals ~ 0 + factor(weekday) + hour:factor(weekday), data = X)
 b1 <- o1$coefficients
 abline(b1[2], b1[7], col=2,lwd=2)
@@ -152,3 +154,26 @@ x <- seq(7,24,by=.01)
 y <- b5[2] + b5[7]*x + b5[12]*x^2 + b5[17]*x^3 + b5[22]*x^4 + b5[27]*x^5
 lines(x,y,col=6,lwd=2)
 legend('topright',as.character(1:5), title = 'order', col = 2:6, lwd=2)
+
+
+attach(workdat)
+#### mixed normal ####
+X1 <- X[X$weekday==1,]
+head(X1)
+plot(0, xlim = c(7,24), ylim = c(min(X1$arrivals), max(X1$arrivals)), 
+     main = 'two-normal for arrival rate function (MONDAYS)',
+     xlab = "Hours", ylab = '# of Arrivals')
+for(i in 1:(nrow(X1)/17)){
+  lines(X1$hour[(i*17-16):(i*17)], X1$arrivals[(i*17-16):(i*17)])
+}
+
+library(minpack.lm)
+o <- nlsLM(arrivals ~ 1/sqrt(2*pi)/s1*exp(-(hour-mu1)^2/(2*s1^2)) 
+         + 1/sqrt(2*pi)/s2*exp(-(hour-mu2)^2/(2*s2^2)), data = X,
+         start=list(mu1=10, s1=1, mu2=15, s2=2), 
+         control = nls.control(maxiter = 100), trace=TRUE)
+b = coef(o)
+x <- seq(7,24,by=.01)
+y1 <- 1/sqrt(2*pi*b[2])*exp(-(x-b[1])^2/2/b[2])
+y2 <- 1/sqrt(2*pi*b[4])*exp(-(x-b[3])^2/2/b[4])
+lines(x,y1,col=3,lwd=2)
